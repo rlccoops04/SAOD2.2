8fdef32c4aca686b01445232db8a6796f80c8629
Булат <@LAPTOP-SSS2TI2M>
2022-05-29 22:02:06 +0300

    Final version

--- theme2.2/theme2.2.cpp
+++ theme2.2/theme2.2.cpp
@@ -2,9 +2,9 @@
 void bubbleSorting(int* arr, int size, int& compares, int& exchanges)
 {
     int temp;
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i < size; i++)
     {
-        for (int j = size - 1; j > i; j--)
+        for (int j = size - 1; j >= i; j--)
         {
             if (arr[j - 1] > arr[j])
             {
@@ -17,73 +17,91 @@ void bubbleSorting(int* arr, int size, int& compares, int& exchanges)
         }
     }
 }
+void insertionSorting(int* arr, int size, int& compares, int& exchanges)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i - 1;
+        while ((++compares) && j >= 0 && arr[i] < arr[j])
+        {
+            j -= 1;
+        }
+        if (j + 1 != i)
+        {
+            int temp = arr[i];
+            int l = i - 1;
+            while (l >= j + 1)
+            {
+                arr[l + 1] = arr[l]; l--;
+                exchanges++;
+            }
+            arr[j + 1] = temp;
+            exchanges++;
+        }
+    }
+    exchanges = exchanges / 3;
+}
 void selectionSorting(int* arr, int size, int& compares, int& exchanges)
 {
-    int temp, k;
+    int temp = 0;
+    int k = 0;
     for (int i = 0; i < size - 1; i++)
     {
         k = i;
-        temp = arr[i];
         for (int j = i + 1; j < size; j++)
         {
-            if (arr[j] < temp)
+            compares++;
+            if (arr[j] < arr[k])
             {
                 k = j;
-                temp = arr[j];
-                exchanges++;
             }
-            compares++;
         }
-        arr[k] = arr[i];
-        arr[i] = temp;
-    }
-}
-void insertionSorting(int* arr, int size, int& compares, int& exchanges)
-{
-    int temp, j;
-    for (int i = 1; i < size; i++)
-    {
-        temp = arr[i];
-        j = i - 1;
-        while ((j >= 0) && (temp < arr[j]))
+        if (i != k)
         {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+            temp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = temp;
             exchanges++;
-            compares++;
         }
-        arr[j + 1] = temp;
-        compares++;
     }
 }
 void ShellSorting(int* arr, int size, int& compares, int& exchanges)
 {
-    int countsteps = (int)log2(size) - 1;
-    int* steps = new int[countsteps];
-    int t = countsteps;
+    int count = round(log2(size) - 1);
+    int* steps = new int[count];
+    int t = count;
     for (int i = 0; t > 0; i++, t--)
     {
         steps[i] = 2 * t - 1;
     }
-    int temp, step, j;
-    for (int numstep = 0; numstep < countsteps; numstep++)
+    int temp, step, j, l;
+    for (int numstep = 0; numstep < count; numstep++)
     {
         step = steps[numstep];
         for (int i = step; i < size; i++)
         {
-            temp = arr[i];
             j = i - step;
-            while ((j >= 0) && (temp < arr[j]))
+            while ((++compares) && (j >= 0) && (arr[i]< arr[j]))
             {
-                arr[j + step] = arr[j];
                 j = j - step;
-                compares++;
+            }
+            if (j + step != i)
+            {
+                temp = arr[i];
+                l = i - step;
+                exchanges++;
+                while (l >= j + step)
+                {
+                    arr[l + step] = arr[l];
+                    l -= step;
+                    exchanges++;
+                }
+                arr[j + step] = temp;
                 exchanges++;
             }
-            compares++;
-            arr[j + step] = temp;
         }
     }
+    exchanges = exchanges / 3;
 }
 void quickSorting(int* arr, int size, int& compares, int& exchanges, int left, int right)
 {
@@ -103,7 +121,6 @@ void quickSorting(int* arr, int size, int& compares, int& exchanges, int left, i
             j--;
             compares++;
         }
-        compares += 2;
         if (i <= j)
         {
             temp = arr[i];
@@ -111,7 +128,7 @@ void quickSorting(int* arr, int size, int& compares, int& exchanges, int left, i
             arr[j] = temp;
             i++;
             j--;
-            exchanges++;
+            exchanges += 3;
         }
     } while (i <= j);
     if (left < j)
@@ -123,29 +140,29 @@ void quickSorting(int* arr, int size, int& compares, int& exchanges, int left, i
         quickSorting(arr, size, compares, exchanges, i, right);
     }
 }
-void Sito(int* arr, int size, int& compares, int& exchanges, int left, int right)
+void Sito(int* arr, int& compares, int& exchanges, int left, int right)
 {
     int i, j, x;
     i = left; j = 2 * left; x = arr[left];
+    exchanges++;
+    compares++;
     if ((j < right) && arr[j + 1] > arr[j])
     {
         j++;
     }
-    compares++;
     while ((j <= right) && (arr[j] > x))
     {
         compares++;
         arr[i] = arr[j];
         exchanges++;
+        compares++;
         i = j;
         j *= 2;
         if ((j < right) && (arr[j + 1] > arr[j]))
         {
             j++;
         }
-        compares++;
     }
-    compares++;
     arr[i] = x;
     exchanges++;
 }
@@ -153,22 +170,25 @@ void Sito(int* arr, int size, int& compares, int& exchanges, int left, int right
 void pyramidSorting(int* arr, int size, int& compares, int& exchanges)
 {
     int temp, left, right;
-    left = (size / 2);
+    left = (size / 2) + 2;
     right = size - 1;
     while (left > 0)
     {
         left--;
-        Sito(arr, size, compares, exchanges, left, right);
+        Sito(arr, compares, exchanges, left, right);
     }
     while (right > 0)
     {
         temp = arr[0];
         arr[0] = arr[right];
         arr[right] = temp;
+        exchanges += 3;
         right--;
-        Sito(arr, size, compares, exchanges, left, right);
+        Sito(arr, compares, exchanges, left, right);
     }
+    exchanges = exchanges / 3;
 }
+
 void duplicateArray(int* mainarr, int* duplarr, int size)
 {
     for (int i = 0; i < size; i++)
@@ -186,7 +206,7 @@ int main()
     int* SortedArr = new int[size];
     for (int i = 0; i < size; i++)
     {
-        MainArray[i] = 0 + rand() % 11;
+        MainArray[i] = 0 + rand() % size;
     }
     duplicateArray(MainArray, SortedArr, size);
     bool menu = true;
@@ -277,6 +297,7 @@ int main()
             {
                 std::cout << SortedArr[i] << " ";
             }
+            exchanges = exchanges / 3;
             std::cout << "\n";
             std::cout << compares << " сравнений, " << exchanges << "перестановок.\n" << std::endl;
             break;
